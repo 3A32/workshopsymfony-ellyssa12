cf43352adc0ba651d4e9f387c41ba52707eff803
ellyssa <ellyssa.khalfaoui@esprit.tn>
2022-10-03 12:07:41 +0100

    03-10-2022 doctrine (db+entity)

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+
+    private ?int $ref = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $titre_ref = null;
+
+    public function getRef(): ?int
+    {
+        return $this->ref;
+    }
+
+    public function getTitreRef(): ?string
+    {
+        return $this->titre_ref;
+    }
+
+    public function setTitreRef(string $titre_ref): self
+    {
+        $this->titre_ref = $titre_ref;
+
+        return $this;
+    }
+    
+}
